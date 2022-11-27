6a6843bbef82ae431d7e751533a3567cb1c05d93
Alex Filip <alexfilippaul@gmail.com>
2022-11-27 17:26:31 +0200

    Add project files.

---
+++ lab1ex2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace lab1ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // media aritmetica a trei numere
+            int a, b, c;
+            float media;
+
+            Console.WriteLine("Introduceti cele 3 numere: ");
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = int.Parse(Console.ReadLine());
+            media = ((float)a + (float)b + (float)c) / 3;
+
+            Console.WriteLine("Media artimetica este " + media);
+        }
+    }
+}
